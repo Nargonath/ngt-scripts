07f2eb6e2932fab8d3f5bd64a398b2ca01b9943c
Jonas Pauthier <jonas.pauthier@gmail.com>
2019-07-26 13:46:43 +0200

    fix(lint-staged): update config to respect new format

--- lint-staged.js
+++ lint-staged.js
@@ -1,12 +1,9 @@
 'use strict';
 
 module.exports = {
-  linters: {
-    // '{*/**/*.js,*.js}': ['prettier --write', 'eslint', 'git add'],
-    '{*/**/*.{js,json,md,html,vue,jsx},*.{md,js,json}}': [
-      'prettier --write',
-      'git add',
-    ],
-  },
-  ignore: ['**/dist', 'CHANGELOG.md'],
+  // '{*/**/*.js,*.js}': ['prettier --write', 'eslint', 'git add'],
+  '{*/**/*.{js,json,md,html,vue,jsx},*.{md,js,json}}': [
+    'prettier --write',
+    'git add',
+  ],
 };
